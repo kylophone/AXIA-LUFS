0c05350aff3d110e90b9feca16c4526fa96109e8
kylophone <kylophone@gmail.com>
2014-09-25 23:24:53 -0500

    Node app for metering in browser.

---
+++ index.js
@@ -0,0 +1,20 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var spawn = require('child_process').spawn;
+var AXIA_LUFS_BIN = "/usr/local/bin/axialufs";
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+axiaLufsJSON = spawn(AXIA_LUFS_BIN);
+
+io.on('connection', function(socket){
+  	axiaLufsJSON.stderr.on('data', function (data) { 					
+  		socket.emit('LUFS', {'shortTerm' : String(data)});
+	});
+});
+
+http.listen(3000, function(){
+});
